1eb1667e894df80826619c4990c23ef477ea112b
Matt Stemper <Matt.Stemper@gmail.com>
2021-11-05 18:32:45 -0500

    removed deprecated python3 method call

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -8,13 +8,13 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
     def test_sell_in_decrements_1_per_update(self):
         items = [Item(name="foo", sell_in=5, quality=1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(4, items[0].sell_in)        
+        self.assertEqual(4, items[0].sell_in)        
 
 if __name__ == '__main__':
     unittest.main()
