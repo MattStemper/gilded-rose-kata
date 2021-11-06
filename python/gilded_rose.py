653655602879a9cbeb6d72a8099bdcb3e786f01b
Matt Stemper <Matt.Stemper@gmail.com>
2021-11-05 19:53:33 -0500

    refactor update logic into methods by item type

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,33 +9,45 @@ class GildedRose(object):
         for item in self.items:
 
             if item.name == "Sulfuras, Hand of Ragnaros":
-                item.quality = 80
+                self.update_sulfuras(item)
 
             elif item.name == "Aged Brie":
-                self.increment_quality(item)
-
-                if item.sell_in <= 0:
-                     self.increment_quality(item)
-
-                self.decrement_sell_in(item)
+                self.update_aged_brie(item)
 
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.increment_quality(item)
-                if item.sell_in < 11 and item.sell_in > 0:
-                    self.increment_quality(item)
-                if item.sell_in < 6 and item.sell_in > 0:
-                    self.increment_quality(item)
-                if item.sell_in <= 0:
-                    item.quality = 0
-
-                self.decrement_sell_in(item)
+                self.update_backstage_pass(item)
 
             else:
-                self.decrement_quality(item)
-                if item.sell_in <= 0:
-                    self.decrement_quality(item)
+                self.update_normal_item(item)
 
-                self.decrement_sell_in(item)
+    def update_normal_item(self, item):
+        self.decrement_quality(item)
+        if item.sell_in <= 0:
+            self.decrement_quality(item)
+
+        self.decrement_sell_in(item)
+
+    def update_backstage_pass(self, item):
+        self.increment_quality(item)
+        if item.sell_in < 11 and item.sell_in > 0:
+            self.increment_quality(item)
+        if item.sell_in < 6 and item.sell_in > 0:
+            self.increment_quality(item)
+        if item.sell_in <= 0:
+            item.quality = 0
+
+        self.decrement_sell_in(item)
+
+    def update_aged_brie(self, item):
+        self.increment_quality(item)
+
+        if item.sell_in <= 0:
+             self.increment_quality(item)
+
+        self.decrement_sell_in(item)
+
+    def update_sulfuras(self, item):
+        item.quality = 80
 
     def decrement_sell_in(self, item):
         item.sell_in = item.sell_in - 1
@@ -49,6 +61,7 @@ class GildedRose(object):
         if item.quality > 0:
             item.quality = item.quality - 1
 
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -57,3 +70,4 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
